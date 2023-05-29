99e7978d9635b27958daeceab7f74971ec266b12
糸 <64469437+yuameshi@users.noreply.github.com>
2023-05-29 09:59:01 +0000

    refactor: use typescript & vite known bugs: cannot save image

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
+import legacy from '@vitejs/plugin-legacy';
+
+export default defineConfig({
+	root: __dirname,
+	clearScreen: false,
+	mode: process.env.NODE_ENV,
+	plugins: [
+		createHtmlPlugin({
+			minify: true,
+		}),
+		legacy({
+			targets: { ie: '11' },
+		}),
+	],
+	base: '/',
+	build: {
+		assetsDir: './assets/',
+		outDir: './dist/',
+		emptyOutDir: true,
+		sourcemap: false,
+		modulePreload: {
+			polyfill: true,
+		},
+		cssCodeSplit: true,
+	},
+});
+
+
